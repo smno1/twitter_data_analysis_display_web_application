21129d1061192b020b54ac8a576ba037e73e6503
sheen <ysmsmno1@gmail.com>
2016-05-04 01:40:37 +1000

    add pie chart for heatmap

--- tweet_analysis/app/assets/javascripts/heatmap.js
+++ tweet_analysis/app/assets/javascripts/heatmap.js
@@ -1,4 +1,7 @@
 var map, heatmap;
+var good_sen=0
+var bad_sen=0
+
 // var markers=[];
 function initHeatMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -17,16 +20,68 @@ function getPoints() {
         type: 'get',
         dataType: 'jsonp',
         success: function(data) {
+          good_sen=0;
+          bad_sen=0;
           for(var i = 0; i < data.rows.length; i++){
             coordinates.push(new google.maps.LatLng(data.rows[i].value[0][0], data.rows[i].value[0][1]));
             sentiments.push(data.rows[i].value[1]);
+            if (data.rows[i].value[1]>0) {
+              good_sen+=1;
+            }else{
+              bad_sen+=1;
+            };
           }
-          setMarkerToPoint(coordinates,sentiments);
+          // setMarkerToPoint(coordinates,sentiments);
+          drawSentimentFanChart();
           heatmap = new google.maps.visualization.HeatmapLayer({
           data: coordinates,
           map: map
         });
         }
+    });
+}
+
+function drawSentimentFanChart(){
+    $('#sentiment_fan').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: '',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 40
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            innerSize: '50%',
+            data: [
+                ['pos',   good_sen/(good_sen+bad_sen)],
+                ['neg',   bad_sen/(good_sen+bad_sen)]
+            ]
+        }]
     });
 }
 
